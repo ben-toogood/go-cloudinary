fcd001717538d527885116cf5d7b1f84cbe9fc37
Kytra <alpha@Bens-MacBook-Pro.local>
2019-08-13 11:23:01 +0100

    Add Transformations

--- transformation.go
+++ transformation.go
@@ -1,9 +1,44 @@
 package cloudinary
 
+import (
+	"fmt"
+	"strings"
+)
+
+// Transformation is a type of image transformation which is supported by cloudinary.
+// docs: https://cloudinary.com/documentation/image_transformation_reference
 type Transformation interface {
-	UrlEncoded() string
+	URLEncoded() string
 }
 
-func (s *Service) TransformImage(publicId string, ts ...Transformation) string {
-	return ""
+// SizeTransformation adjusts the size of an image
+type SizeTransformation struct {
+	Width  int
+	Height int
+}
+
+// URLEncoded returns the URL path component required for transformation
+func (t SizeTransformation) URLEncoded() string {
+	var components []string
+
+	if t.Width > 0 {
+		components = append(components, fmt.Sprintf("w_%v", t.Width))
+	}
+
+	if t.Height > 0 {
+		components = append(components, fmt.Sprintf("h_%v", t.Height))
+	}
+
+	return strings.Join(components, ",")
+}
+
+// TransformedImageURL returns the URL for an image, having applied the transformationsfs
+func (s *Service) TransformedImageURL(publicID string, ts ...Transformation) string {
+	segments := make([]string, len(ts))
+	for i, t := range ts {
+		segments[i] = t.URLEncoded()
+	}
+
+	joinedSegments := strings.Join(segments, "/")
+	return fmt.Sprintf("%s/%s/%s/upload/%s/%s", baseResourceUrl, s.cloudName, imageType, joinedSegments, publicID)
 }
--- transformation_test.go
+++ transformation_test.go
@@ -1,7 +1,52 @@
 package cloudinary
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestTransformImage(t *testing.T) {
+	srv := Service{cloudName: "demo"}
+	publicID := "yellow_tulip.jpg"
 
+	tt := []struct {
+		name            string
+		url             string
+		transformations []Transformation
+	}{
+		{
+			name: "No transformations",
+			url:  fmt.Sprintf("%v/%v/image/upload//%v", baseResourceUrl, srv.CloudName(), publicID),
+		},
+		{
+			name:            "Height transformation",
+			transformations: []Transformation{SizeTransformation{Height: 100}},
+			url:             fmt.Sprintf("%v/%v/image/upload/h_100/%v", baseResourceUrl, srv.CloudName(), publicID),
+		},
+		{
+			name:            "Width transformation",
+			transformations: []Transformation{SizeTransformation{Width: 100}},
+			url:             fmt.Sprintf("%v/%v/image/upload/w_100/%v", baseResourceUrl, srv.CloudName(), publicID),
+		},
+		{
+			name:            "Width and height transformations",
+			transformations: []Transformation{SizeTransformation{Height: 100, Width: 100}},
+			url:             fmt.Sprintf("%v/%v/image/upload/w_100,h_100/%v", baseResourceUrl, srv.CloudName(), publicID),
+		},
+		{
+			name:            "Width and invalid height transformations",
+			transformations: []Transformation{SizeTransformation{Height: -100, Width: 100}},
+			url:             fmt.Sprintf("%v/%v/image/upload/w_100/%v", baseResourceUrl, srv.CloudName(), publicID),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			url := srv.TransformedImageURL(publicID, tc.transformations...)
+
+			if url != tc.url {
+				t.Errorf("Expected to get %v but actually got %v", tc.url, url)
+			}
+		})
+	}
 }
